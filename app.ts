c4b110dc2f644af169c91b6a3d6a1d54a15a63a0
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-08 15:36:25 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,75 @@
+import add from "./index.js"
+console.log(add(9,9));
+            ///////////////examples//////////////
+//import sub from "./index.js"
+//console.log(sub(1,1,1,1));
+
+//import { mult } from "./index.js";
+//console.log(mult(6,4));
+
+//import { div } from "./index.js";
+//console.log(div(5,2));
+
+
+//import { userNames, myChoice, favFlower, favDress } from "./index.js";
+//console.log(userNames);
+                             ///////////////examples//////////////
+//import { myChoice } from "./index.js";
+//console.log(myChoice);
+
+                          
+//import { favFlower } from "./index.js";
+//console.log(favFlower);
+                       ///////////////example///////////////
+//import { favDress } from "./index.js";
+//console.log(favDress);
+
+//import { obj } from "./index.js";
+//console.log(obj);
+                          ///////////////example///////////////
+//import { myName } from "./index.js";
+///console.log(myName);
+
+                         //////////////step03a_modules example///////////
+     // import { Name as you } from "./index.js";   
+     // console.log(you.status);    
+      
+      
+  // import { userNames,favFlower } from "./index.js";
+   //console.log(userNames,favFlower);
+
+
+
+                    ///////////////////////step03c import inquirer////////////////////////////
+   //import inquirer from "inquirer";
+
+  // let saba = await inquirer.prompt({
+       // name: "name",
+       // type: "input",
+       // message: "Please Enter Your Name...",
+  // })
+  // console.log(`Hello! ${saba.name}`);
+    
+
+                       //////////////////////step03d import chalk examples////////////////////////////
+   //import inquirer from "inquirer";
+//import chalk from "chalk";
+
+//let answers = await inquirer.prompt([{
+    //name: "age",
+   // type: "number",
+   // message: "Enter your Age:"}
+//]);
+
+//console.log(chalk.blue("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old."));
+
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+   let saba = await inquirer.prompt([{
+      name: "saba",
+      type: "input",
+      message: chalk.greenBright("Tell me about ypurself....?")
+   }]);
+   console.log(`Hello! ${saba.name}`);
---
+++ index.js
@@ -0,0 +1,61 @@
+export default add;
+///////////////////functions/////////////////////
+function add(num1, num2) {
+    return num1 + num2;
+}
+;
+///////////////examples//////////////
+// export default sub;
+//function sub(num1:number, num2:number, num3:number, num4:number){
+//  return num1 - num2 - num3 - num4
+//};
+//export function mult(num1:number, num2:number){
+// return num1 * num2
+//};
+//export function div(num1:number, num2:number){
+//return num1 / num2
+//};
+///////////////Array///////////////////
+//export let userNames : string[] = ["Saba", "Dua", "riffat"];
+///////////////example///////////////
+// export let myChoice : string[] = ["Rose", "Pineapple", "SabaNizam", "United states"]
+//////////////variable/////////////////
+//export let favFlower = "Rose"
+///////////////example///////////////
+//export let favDress = "abaya"
+///////////////////object/////////////////////                    
+// export let obj ={
+//name: "Saba",
+// age:  "26",
+// gender: "female",
+// education: "Masters",
+// mobile:  "090078601",
+//}    
+///////////////example///////////////
+//export let myName ={
+//   name: "Dua",
+//  age: "17",
+// gender: "female",
+//  education: "intermediate",
+//  city: "karachi",
+// country: "pakistan",
+// status: "IT student",
+// phone: "090078601",
+///  subject: "computer science student",
+//}
+//////////////step03a_modules example///////////
+//export let Name ={
+//// name: "Dua",
+//age: "17",
+// gender: "female",
+//education: "intermediate",
+//city: "karachi",
+// country: "pakistan",
+// status: "IT student",
+// phone: "090078601",
+// subject: "computer science student",
+//}
+///////////one time export two or more lines/////////////////////
+//let userNames : string[] = ["Saba", "Dua", "riffat"];
+//let favFlower = "Rose"
+//export {userNames, favFlower};
